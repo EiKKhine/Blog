37221e6b70cbfca7b4b119b08afa2d2314f5446b
Tr-May <may.passionateteacher@gmail.com>
2024-11-03 12:32:31 +0630

    starter template with google font, bootstrap and fontawesome

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGauge } from "@fortawesome/free-solid-svg-icons";
+
+const App = () => {
+  return (
+    <div>
+      <FontAwesomeIcon icon={faGauge} />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quas,
+        sint modi et exercitationem quis? Non velit aliquid adipisci saepe
+        libero nesciunt beatae, quis earum accusantium, quibusdam sed
+        consequuntur maiores.
+      </p>
+    </div>
+  );
+};
+
+export default App;
